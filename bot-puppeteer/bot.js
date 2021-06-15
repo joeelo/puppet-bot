668b66ac5bab900e391bd811b4a1117d32432d03
joeelo <josephclorenzo@gmail.com>
2021-06-15 13:30:19 -0700

    only need last button

--- bot-puppeteer/bot.js
+++ bot-puppeteer/bot.js
@@ -1,7 +1,19 @@
 const pup = require('puppeteer');
+const card = require('./credit');
 
 // const productUrl = 'https://www.walmart.com/ip/Sony-PlayStation-5-Digital-Edition/493824815';
-const productUrl = 'https://www.walmart.com/ip/Everso-Men-s-Luxury-Watch-Maserati-Watch-Waterproof-Watch-Men-s-Business-Stainless-Sports-Watch-Quartz-Watch/796779918'
+const productUrl = 'https://www.walmart.com/ip/Everso-Men-s-Luxury-Watch-Maserati-Watch-Waterproof-Watch-Men-s-Business-Stainless-Sports-Watch-Quartz-Watch/796779918'; 
+
+const clearAndFill = async (page, inputId, text) => {
+	try {
+		const input = await page.$(inputId); 
+		await input.click({ clickCount: 3 });
+		await input.type(text);
+		await page.keyboard.press('Tab');
+	} catch (error) {
+        console.log("🚀 ~ file: bot.js ~ line 11 ~ clearAndFill ~ error", error);
+	}
+}
 
 const openPage = async () => {
 	try {
@@ -27,7 +39,7 @@ const addToCart = async (page) => {
 		await page.waitForSelector('[data-automation-id="fulfillment-continue"]');
 		await page.click('button[data-automation-id="fulfillment-continue"]', elem => elem.click());
 	} catch (error) {
-    console.log("🚀 ~ file: bot.js ~ line 22 ~ addToCart ~ error", error);
+		console.log("🚀 ~ file: bot.js ~ line 22 ~ addToCart ~ error", error);
 	}
 }
 
@@ -42,18 +54,37 @@ const fillOutBillingSection = async (page) => {
 		await page.waitForTimeout(1000);
 		await page.type('input[id="email"]', 'josephclorenzo@gmail.com');
 		await page.waitForTimeout(1000);
-		await page.type('input[id="addressLineOne"]', '801 A street');
+		const selectElem = await page.$('select[id="state"]'); 
+		await selectElem.type('California');
+		await page.type('input[id="addressLineOne"]', '801 a st');
 		await page.waitForTimeout(1000);
-		await page.type('input[id="addressLineTwo"]', 'Apt 2019');
-		await page.waitForTimeout(1000);
-		await page.type('input[id="city"]', 'San Diego');
-		await page.waitForTimeout(1000);
-		await page.select('input[id="state"]', 'California');
-		await page.waitForTimeout(1000);
-		await page.select('input[id="postalCode"]', '92101');
-		await page.waitForTimeout(1000);
+		await page.keyboard.press('Tab');
+		await page.type('input[id="addressLineTwo"]', '#2019');
+		await page.click('button[data-automation-id="address-book-action-buttons-on-continue"]');
 	} catch (error) {
-        console.log("🚀 ~ file: bot.js ~ line 38 ~ fillOutBillingSection ~ error", error)
+		console.log("🚀 ~ file: bot.js ~ line 38 ~ fillOutBillingSection ~ error", error);
+	}
+}
+
+const fillOutCardSection = async (page) => {
+	try {
+		await page.waitForSelector('input[data-automation-id="cardNumber-cc"]');
+		await page.type('input[data-automation-id="cardNumber-cc"]', card.cardNumber);
+		const monthSelect = await page.$('select[data-automation-id="expiryMonth-cc"]');
+		await monthSelect.type(card.expMonth);
+		const yearSelect = await page.$('select[data-automation-id="expiryYear-cc"]');
+		await yearSelect.type(card.expYear);
+		await page.type('input[data-automation-id="cvv-verify-cc"]', card.ccv); 
+		await page.click('input[class="input-toggle__input"]'); 
+		await clearAndFill(page, 'input[data-automation-id="addressLineOne-cc"]', '2842 Grand Concourse')
+		await page.type('input[data-automation-id="addressLineTwo-cc"]', 'Apt 6H');
+		await clearAndFill(page, 'input[data-automation-id="city-cc"]', 'Bronx');
+		await clearAndFill(page, 'input[data-automation-id="postalCode-cc"]', '10458');
+		const citySelect = await page.$('select[id="state"]');
+		citySelect.type('New York');
+		await page.click('button[data-automation-id="save-cc"]');
+	} catch (error) {
+        console.log("🚀 ~ file: bot.js ~ line 62 ~ fillOutCardSection ~ error", error);
 	}
 }
 
@@ -62,6 +93,7 @@ const checkout = async () => {
 		const openedPage = await openPage(); 
 		await addToCart(openedPage);
 		await fillOutBillingSection(openedPage);
+		await fillOutCardSection(openedPage);
 	} catch (error) {
         console.log("🚀 ~ file: bot.js ~ line 13 ~ createPage ~ error", error);
 	}
